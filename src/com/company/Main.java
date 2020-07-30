a0eb2e3f5944058b17a3aa3c5cbc73dace6c1a16
edgar0696 <oostreetworkoutoo@gmail.com>
2020-07-30 03:09:03 +0300

    OOPTrapezoid

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,9 +12,21 @@ public class Main {
         int area = (lenghtA + lenghtB) / 2 * height;
         System.out.println("Trapezium area = " + area);
 
-        Trapezoid t1 = new Trapezoid(11,5,7);
+        System.out.println("");
+        System.out.println("                ↓--------------- Constructor class --------------↓");
+
+        Trapezoid t1 = new Trapezoid(11,5,7,8,12);
             int areaT1 = t1.getArea();
         System.out.println("Trapezium area = " + areaT1);
+
+        Trapezoid t2 = new Trapezoid(15,7,12,13,17);
+        int areaT2 = t2.getArea();
+        System.out.println("Trapezium area = " + areaT2);
+        System.out.println("Пример как вывести areaT1 и areaT2 за один раз - " + areaT1 + " and " + areaT2);
+
+        System.out.println();
+        System.out.println("                  ↓--------------- Class method --------------↓");
+        System.out.println("Perimeter of the trapezoid = " + t2.getPerimetr());
 
 
     }
--- src/com/company/Trapezoid.java
+++ src/com/company/Trapezoid.java
@@ -4,13 +4,22 @@ public class Trapezoid {
     private int lenghtA;
     private int lenghtB;
     private int height;
+    private int widthA;
+    private int widthB;
 
-    public Trapezoid(int lenghtA, int lenghtB, int height) {
+    public Trapezoid(int lenghtA, int lenghtB, int height, int widthA, int widthB) {
         this.lenghtA = lenghtA;
         this.lenghtB = lenghtB;
         this.height = height;
+        this.widthA = widthA;
+        this.widthB = widthB;
     }
-    int getArea(){
+
+    public int getArea(){
         return (lenghtA + lenghtB) / 2 * height;
+    }
+
+    public int getPerimetr (){
+        return lenghtA + lenghtB + widthA + widthB;
     }
 }
